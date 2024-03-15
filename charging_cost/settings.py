4d875971a45ab1750f287cc54782b26b27e81864
Michał Ćwikliński <michal@cwiklin.ski>
2024-03-15 20:05:51 +0100

    Added static files configuration

--- charging_cost/settings.py
+++ charging_cost/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'static-files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
