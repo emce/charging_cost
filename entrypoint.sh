53abcdca01ff4c065fb9655ae590ef004d0f3860
Michał Ćwikliński <michal@cwiklin.ski>
2024-03-15 20:15:09 +0100

    Fix for wsgi config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
-gunicorn -b 0.0.0.0:9080 charging.wsgi --timeout 200 --workers=5
+gunicorn -b 0.0.0.0:9080 charging_cost.wsgi --timeout 200 --workers=5
