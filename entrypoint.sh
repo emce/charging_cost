462822da3fbbb2b946fa6bb05272a44a75e39a2c
Michał Ćwikliński <michal@cwiklin.ski>
2024-03-16 21:50:06 +0100

    Updates for doker configuration #6

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+touch .env
+echo "DEBUG=0" >> .env
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 gunicorn -b 0.0.0.0:9080 charging_cost.wsgi --timeout 200 --workers=5
